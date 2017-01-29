7afb266fa23f4bfd5dfaab91f64b0c1c6f8abed8
Deleetdk <emil@emilkirkegaard.dk>
2017-01-29 02:39:15 +0100

    tests for GG_scatter

--- tests/testthat/test_plots.R
+++ tests/testthat/test_plots.R
@@ -22,3 +22,16 @@ test_that("denhist", {
 })
 
 
+
+# GG_scatter --------------------------------------------------------------
+
+
+test_that("scatter", {
+  expect_s3_class(GG_scatter(iris, "Sepal.Length", "Sepal.Width"), "ggplot")
+  expect_s3_class(GG_scatter(iris, "Sepal.Length", "Sepal.Width", case_names_vector = rep("A", 150)), "ggplot")
+  expect_s3_class(GG_scatter(iris, "Sepal.Length", "Sepal.Width", case_names_vector = "Species"), "ggplot")
+  expect_s3_class(GG_scatter(iris, "Sepal.Length", "Sepal.Width", text_pos = "br"), "ggplot")
+  expect_s3_class(GG_scatter(iris, "Sepal.Length", "Sepal.Width", CI = .99), "ggplot")
+  expect_s3_class(GG_scatter(iris, "Sepal.Length", "Sepal.Width", clean_names = F), "ggplot")
+  expect_s3_class(GG_scatter(iris, "Sepal.Length", "Sepal.Width", weights = 1:150), "ggplot")
+})
