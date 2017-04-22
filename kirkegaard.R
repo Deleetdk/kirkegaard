6ff47d83e6c51935cd65bf5aecdb1f7d5c4ee9c1
Emil O. W. Kirkegaard <emil@emilkirkegaard.dk>
2017-04-22 09:44:01 +0200

    Better code for installing dependencies

--- kirkegaard.R
+++ kirkegaard.R
@@ -1,5 +1,8 @@
 #libs
-library(pacman)
+if (!require("pacman")) {
+  install.packages("pacman")
+  library("pacman")
+}
 p_load(devtools, roxygen2, stringr, testthat)
 options(digits = 2, scipen = 2)
 
@@ -7,9 +10,26 @@ options(digits = 2, scipen = 2)
 document()
 
 #install
+#for some weird reason this sometimes begins reinstalling packages for no reason I can find
 #install("../kirkegaard")
 install("../kirkegaard", upgrade_dependencies = F)
 
 #load
 library(kirkegaard)
 
+
+# installing from scratch -------------------------------------------------
+#for whatever reason, it is hard to get it to install the dependencies automatically
+#so here's a small call to manually install them
+
+if (F) {
+  #pacman
+  if (require("pacman")) install.packages("pacman")
+
+  #CRAN packages
+  library(pacman)
+  p_load(grid, ggplot2, scales, stringr, purrr, assertthat, readr, xml2, plyr, dplyr, tidyr, psych, gtools, robustbase, MASS, forcats, polycor, weights, devtools, VIM, lsr, compute.es, magrittr, tibble, psychometric, Hmisc, XLConnect, stringdist, geosphere, fields, rmngb, ape, glmnet)
+
+  #github packages
+  purrr::map(c("thomasp85/curry"), ~devtools::install_github(.))
+}
