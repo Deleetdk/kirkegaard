d91483c382f1dbc13784f30af518c8b68e24a02f
Emil O. W. Kirkegaard <emil@emilkirkegaard.dk>
2017-10-17 12:17:57 -0400

    tests for rescale()

---
+++ tests/testthat/test_simple_math.R
@@ -0,0 +1,23 @@
+### tests for df_ functions
+
+context("simple_math")
+
+# rescale ------------------------------------------------------------------
+
+test_that("df_standardize", {
+  #basic
+  expect_true(min_max(rescale(1:10, new_min = 0, new_max = 1)) %equals% c(0, 1))
+  expect_true(min_max(rescale(1:10, new_min = 1, new_max = 5)) %equals% c(1, 5))
+  expect_true(min_max(rescale(rnorm(100), new_min = 1, new_max = 0)) %equals% c(0, 1))
+
+  #complex
+  #assume old numbers belong to scale 0-1, rescale to 10-20 scale, i.e. 11, 15, 20
+  expect_true(all(rescale(c(.1, .5, 1), new_min = 10, new_max = 20, old_min = 0, old_max = 1) == c(11, 15, 20)))
+
+  #NAs
+  expect_true(min_max(rescale(c(1:5, NA, 6:10), 0, 1)) %equals% c(0, 1))
+
+  #empty input
+  expect_true(rescale(numeric(), 0, 1) %equals% numeric())
+})
+
