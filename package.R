d926461097294b958ab1a6fb940c113172e37163
Emil O. W. Kirkegaard <emil@emilkirkegaard.dk>
2020-05-15 14:11:35 +0200

    remove duplicated code
    this code is now in README.md

--- package.R
+++ package.R
@@ -17,21 +17,3 @@ devtools::install("../kirkegaard")
 #load
 pacman::p_load(kirkegaard, testthat)
 
-
-# installing from scratch -------------------------------------------------
-#for whatever reason, it is sometimes hard to get it to install the dependencies automatically
-#so here's a small call to manually install them
-
-if (F) {
-  #pacman
-  if (require("pacman")) install.packages("pacman")
-
-  #CRAN packages
-  library(pacman)
-  p_load(grid, ggplot2, scales, stringr, purrr, assertthat, readr, xml2, plyr, dplyr, tidyr, psych, gtools, robustbase, MASS, forcats, polycor, weights, devtools, VIM, lsr, compute.es, magrittr, tibble, psychometric, Hmisc, stringdist, geosphere, fields, rmngb, ape, glmnet, metafor, binom, ggrepel, Rcsdp, readxl)
-
-  #finally
-  devtools::install_github("deleetdk/kirkegaard")
-}
-
-#test
