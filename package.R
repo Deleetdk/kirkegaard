570b634baf817e129981c475a8ae6abb9026e6bb
Emil Kirkegaard <emil@emilkirkegaard.dk>
2024-05-03 03:23:07 +0200

    added code to package.R to make installation easier

--- package.R
+++ package.R
@@ -23,3 +23,43 @@ devtools::install("../kirkegaard", dependencies = F)
 library(kirkegaard)
 library(testthat)
 
+#first time installation
+if (F) {
+  # Function to extract packages from .R files
+  find_used_packages <- function(file_paths) {
+    # Container for all identified packages
+    all_packages <- character()
+
+    # Loop over each file
+    ns_names = c()
+    for (file in file_paths) {
+      # Read the file
+      lines <- readLines(file, warn = FALSE)
+
+      # Find usage of explicit namespace prefixing
+      ns_usage <- grep("::", lines, value = TRUE)
+      if (length(ns_usage) > 0) {
+
+        # Extract namespace names
+        ns_names <- sapply(regmatches(ns_usage, gregexpr("\\b[a-zA-Z0-9.]+::", ns_usage)), function(x) gsub("::", "", x)) |> unlist() |>
+          unique()
+        #add to collection
+        all_packages = c(all_packages, ns_names)
+      }
+    }
+
+    # Remove duplicates and return
+    return(unique(all_packages))
+  }
+
+  #scan all .R files, and add tidyverse
+  (kirkegaard_packages = c(find_used_packages(dir(pattern = ".R$", recursive = T, full.names = T)), "tidyverse"))
+
+  #not installed packages
+  (not_installed_packages = kirkegaard_packages[!(kirkegaard_packages %in% installed.packages()[,"Package"])] |> setdiff("kirkegaard"))
+
+  #install them
+  if (length(not_installed_packages) > 0) {
+    install.packages(not_installed_packages)
+  }
+}
